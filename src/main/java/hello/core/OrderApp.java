a5baa036026cc03efabbc5a4693c7c6685c2032f
jinsik_park <jinsik_park@tmax.co.kr>
2022-08-29 13:31:54 +0900

    .

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -20,6 +20,10 @@ public class OrderApp {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+        String id = applicationContext.getId();
+        String displayName = applicationContext.getDisplayName();
+        System.out.println(id);
+        System.out.println(displayName);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
